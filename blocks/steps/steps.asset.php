184a719c1f70c1e478850d454ac3349858075c44
Radley <radley@alchemyandaim.com>
2023-08-15 13:19:06 -0700

    Updated version and fixed warning

--- blocks/steps/steps.asset.php
+++ blocks/steps/steps.asset.php
@@ -1,5 +1,5 @@
 <?php
 // this type of file is usually compiled using wp-scripts in npm/node. however, this is not the case here.
 // this file is not compiled and instead just a regular js file
-$v = filemtime('steps.js');
+$v = filemtime(__DIR__ . '/steps.js');
 return array('dependencies' => array(), 'version' => $v);
